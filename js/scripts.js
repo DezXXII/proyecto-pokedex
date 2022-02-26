53f498d4c79ef2c081879e2d5efe7e333dea2057
Matias <mattkramm@hotmail.com>
2022-02-26 15:17:22 -0300

    Se pueden dar vuelta las 'cartas', se muestran los tipos y stats de los pokemon, ajustes al boton de borrar todos los pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 const pokemonContainer = document.querySelector('.pokemon-container');
+const searchedPokemonContainer = document.querySelector('.searched-pokemon-container');
 
 // Section: Fetching pokemons
 
-function fetchPokemon(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+async function fetchPokemon(id) {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
     .then(data => {
         createPokemon(data);
         pokemons.push(data.name);
+        console.log(data)
     })
 }
 
@@ -22,12 +24,14 @@ const nextPaginationButton = document.querySelector('.nextBtn');
 previousPaginationButton.addEventListener('click', (e) => {
     if (offset != 1) { 
         offset -= 9;
+        removeChildNodes(pokemonContainer)
         fetchPokemons(offset, limit);
     }
 })
 
 nextPaginationButton.addEventListener('click', (e) => {
     offset += 9;
+    removeChildNodes(pokemonContainer)
     fetchPokemons(offset, limit);
 
     previousPaginationButton.style.display = "block";
@@ -42,29 +46,30 @@ function fetchPokemons(offset, limit) {
 
 // Section: Fetching pokemons by name
 
-function fetchPokemonByName(name) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+async function fetchPokemonByName(name) {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
     .then(res => res.json())
     .then(data => {
-        createPokemon(data);
+        searchedPokemon(data);
         pokemons.push(data.name);
     })
 }
 
-let pokemons = []
+let pokemons = [];
 
 const pokemonInput = document.querySelector('.pokemon-input')
-const pokemonInputValue = document.querySelector('.pokemon-input').value
 const searchBar = document.querySelector('.search-bar')
 
 searchBar.addEventListener('submit', (e) => {
     e.preventDefault()
 
     if(pokemons.includes(pokemonInput.value.toLowerCase())) {
-        alert('Ese pokemon ya está en pantalla');
+        alert('That pokemon is already on the screen');
     } else {
         fetchPokemonByName(pokemonInput.value.toLowerCase())
     }
+
+    pokemonInput.value = '';
 })
 
 // Section: Deleting all pokemons
@@ -77,15 +82,118 @@ btnDeletePokemons.addEventListener('click', (e) => {
 
     offset = -8;
 
-    const pokemonContainer = document.querySelector('.pokemon-container')
-    pokemonContainer.innerHTML = '';
-
-    previousPaginationButton.style.display = "none";
-    nextPaginationButton.textContent = "Show Pokemons";
+    searchedPokemonContainer.innerHTML = '';
 })
+
+// Section: Creating the pokemon container based on the search bar result
+
+
+
+function searchedPokemon(pokemon) {
+
+    const searchedPokemonsLength = document.querySelector('.searched-pokemon-container').childElementCount;
+
+    if(searchedPokemonsLength < 5) {
+        const flipCard = document.createElement('div');
+        flipCard.classList.add('flip-card');
+
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container')
+
+        flipCard.appendChild(cardContainer);
+
+        const card = document.createElement('div')
+        card.classList.add('pokemon-block');
+    
+        const spriteContainer = document.createElement('div')
+        spriteContainer.classList.add('img-container');
+    
+        const sprite = document.createElement('img');
+        sprite.src = pokemon.sprites.other.home.front_default;
+    
+        spriteContainer.appendChild(sprite);
+    
+        const number = document.createElement('p');
+        number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
+    
+        const name = document.createElement('p');
+        name.classList.add('name');
+        name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    
+        card.appendChild(spriteContainer);
+        card.appendChild(number);
+        card.appendChild(name);
+
+        const cardBack = document.createElement('div');
+
+        const cardBackTitle = document.createElement('p');
+        cardBackTitle.textContent = "Pokemon Stats";
+
+        cardBack.classList.add('pokemon-block-back');
+        cardBack.appendChild(cardBackTitle)
+    
+        cardContainer.appendChild(card);
+        cardContainer.appendChild(cardBack);
+        searchedPokemonContainer.appendChild(flipCard);
+
+        // Stats
+
+        const statsContainer = document.createElement('div');
+        statsContainer.classList.add('stats');
+
+        const statType1 = document.createElement('p')
+        statType1.textContent = 'Tipo: ' + pokemon.types[0].type.name;
+        
+        const statType2 = document.createElement('p')
+
+        if(pokemon.types.length > 1) {
+            statType2.textContent = 'Tipo: ' + pokemon.types[1].type.name;
+        }
+
+        const statHp = document.createElement('p')
+        statHp.textContent = 'HP: ' + pokemon.stats[0].base_stat;
+
+        // const statAttack = document.createElement('p')
+        // statAttack.textContent = 'Attack: ' +
+
+        // const statDefense = document.createElement('p')
+        // statDefense.textContent = 'Defense: ' +
+
+        // const statSpecialAttack = document.createElement('p')
+        // statSpecialAttack.textContent = 'Special Attack: ' +
+
+        // const statSpecialDefense = document.createElement('p')
+        // statSpecialDefense = 'Special Defense: ' + 
+
+        // const statSpeed = document.createElement('p')
+        // statSpeed.textContent = 'Speed: ' + 
+
+        statsContainer.appendChild(statType1)
+        statsContainer.appendChild(statType2)
+        statsContainer.appendChild(statHp);
+
+        cardBack.appendChild(statsContainer);
+    }
+}
+
 // Section: Creating the pokemons containers with their corresponding data
 
 function createPokemon(pokemon) {
+
+    // ******************************************************************************** //
+    // Flip Card
+
+    const flipCard = document.createElement('div');
+    flipCard.classList.add('flip-card');
+
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
+
+    flipCard.appendChild(cardContainer);
+
+    // ******************************************************************************** //
+    // Creating the container
+
     const card = document.createElement('div')
     card.classList.add('pokemon-block');
 
@@ -93,7 +201,7 @@ function createPokemon(pokemon) {
     spriteContainer.classList.add('img-container');
 
     const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.front_default
+    sprite.src = pokemon.sprites.other.home.front_default;
 
     spriteContainer.appendChild(sprite);
 
@@ -108,7 +216,84 @@ function createPokemon(pokemon) {
     card.appendChild(number);
     card.appendChild(name);
 
-    pokemonContainer.appendChild(card);
+    // ******************************************************************************** //
+    // Back Card
+
+    const cardBack = document.createElement('div');
+
+    const cardBackTitle = document.createElement('p');
+    cardBackTitle.textContent = "Pokemon Stats";
+
+    cardBack.classList.add('pokemon-block-back');
+    cardBack.appendChild(cardBackTitle)
+
+    cardContainer.appendChild(card);
+    cardContainer.appendChild(cardBack);
+    pokemonContainer.appendChild(flipCard);
+
+    // ******************************************************************************** //
+    // Stats
+
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add('stats');
+
+    const typesContainer = document.createElement('div');
+    typesContainer.classList.add('types');
+
+    // Stats (Types)
+
+    const statType1 = document.createElement('p')
+    statType1.textContent = pokemon.types[0].type.name;
+    
+    const statType2 = document.createElement('p')
+
+    if(pokemon.types.length > 1) {
+        statType2.textContent = pokemon.types[1].type.name;
+    }
+
+    // ******************************************************************************** //
+    // Stats (Stats)
+
+    const statHp = document.createElement('p')
+    statHp.textContent = 'HP: ' + pokemon.stats[0].base_stat;
+
+    const statAttack = document.createElement('p')
+    statAttack.textContent = 'Attack: ' + pokemon.stats[1].base_stat;
+
+    const statDefense = document.createElement('p')
+    statDefense.textContent = 'Defense: ' + pokemon.stats[2].base_stat;
+
+    const statSpecialAttack = document.createElement('p')
+    statSpecialAttack.textContent = 'Special Attack: ' + pokemon.stats[3].base_stat;
+
+    const statSpecialDefense = document.createElement('p')
+    statSpecialDefense.textContent = 'Special Defense: ' + pokemon.stats[4].base_stat;
+
+    const statSpeed = document.createElement('p')
+    statSpeed.textContent = 'Speed: ' + pokemon.stats[5].base_stat;
+
+    // Types Container
+
+    typesContainer.appendChild(statType1)
+    typesContainer.appendChild(statType2)
+
+    // Stats Container
+
+    statsContainer.appendChild(statHp);
+    statsContainer.appendChild(statAttack);
+    statsContainer.appendChild(statDefense);
+    statsContainer.appendChild(statSpecialAttack);
+    statsContainer.appendChild(statSpecialDefense);
+    statsContainer.appendChild(statSpeed);
+
+    cardBack.appendChild(typesContainer);
+    cardBack.appendChild(statsContainer);
+}
+
+function removeChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
 }
 
 fetchPokemons(offset, limit);
